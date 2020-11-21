c5edf401e17f311ed46a75d02266eb880c86c88a
Golikov-Andrew <andrejgolikov46@gmail.com>
2020-11-21 17:41:58 +0300

    Bugfix.

--- Sentry.py
+++ Sentry.py
@@ -11,21 +11,25 @@ class Sentry:
 		header = ['is_enabled', 'minute', 'hour', 'day_of_week']
 		data = list()
 		data.append(header)
-		min_per_day = 1440
-		init_min = 0
-		init_hour = 0
-		hours = min_per_day / interval_min
-		for dow in range(7):
-			for h in range(int(hours)):
-				data.append([1, init_min, init_hour, dow + 1])
-				init_min += interval_min
-				dif = 60 - init_min
-				if dif <= 0:
-					init_min = -dif
-					init_hour += 1
-					dif_h = 24 - init_hour
-					if dif_h <= 0:
-						init_hour = -dif_h
+
+		min = 0
+		hour = 0
+		dow = 0
+
+		while True:
+			data.append([1, min, hour, dow + 1])
+			min += interval_min
+			dif = 60 - min
+			if dif <= 0:
+				min = -dif
+				hour += 1
+				dif_h = 24 - hour
+				if dif_h <= 0:
+					hour = -dif_h
+					dow += 1
+					dif_dow = 7 - dow
+					if dif_dow <= 0:
+						break
 
 		with open(dest_file_path, 'w', encoding='utf-8') as f:
 			csv_writer = csv.writer(f, delimiter=';')
@@ -36,7 +40,7 @@ class Sentry:
 if __name__ == '__main__':
 	sentry = Sentry()
 	cur_dir = os.path.abspath(os.curdir)
-	grafic_file_path = os.path.join(cur_dir, 'shedules', 'grafic.csv')
+	grafic_file_path = os.path.join(cur_dir, 'shedules', 'grafic_2.csv')
 	interval_min = 15
 	sentry.generate_shedule(grafic_file_path, interval_min)
 
